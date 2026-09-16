8826faf581fe94e1165d7475c69d937426a33b1f
matt <hwqla@foxmail.com>
2026-09-16 14:26:49 +0800

    Operator Schema and Shape inference

--- examples/quickstart.cpp
+++ examples/quickstart.cpp
@@ -6,7 +6,7 @@ int main() {
     tinyinfer::Graph graph;
     const tinyinfer::TensorSpec spec{{1, 4}, tinyinfer::DataType::Float32};
     const auto x = graph.add_input("input", spec);
-    graph.add_node("relu", tinyinfer::OpType::ReLU, {x}, {spec});
+    graph.add_node("relu", tinyinfer::OpType::ReLU, {x});
     tinyinfer::CpuBackend cpu;
     std::cout << "TinyInfer scaffold: tensor has " << input.numel() << " elements, graph has "
               << graph.size() << " nodes, backend is " << cpu.name() << ".\n";
